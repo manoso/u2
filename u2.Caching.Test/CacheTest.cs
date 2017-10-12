5e3b0682fe57c37769073e9aca0e4461638c4a48
Tong Man <au.tong.man@gmail.com>
2017-10-12 22:40:55 +1100

    unit tests

---
+++ u2.Caching.Test/CacheTest.cs
@@ -0,0 +1,39 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using u2.Core.Contract;
+using u2.Test;
+
+namespace u2.Caching.Test
+{
+    [TestFixture]
+    public class CacheTest
+    {
+        [Test]
+        public void FetchAsync_without_key_success()
+        {
+            var cacheStore = Substitute.For<ICacheStore>();
+            var cacheRegistry = Substitute.For<ICacheRegistry>();
+
+            var cache = new Cache(cacheStore, cacheRegistry);
+
+            cache.FetchAsync<TestItem>();
+
+            var key = typeof(TestItem).FullName;
+            cacheRegistry.Received(1).TryGetTask(key, out ICacheTask _);
+        }
+
+        [Test]
+        public void FetchAsync_with_key_success()
+        {
+            var cacheStore = Substitute.For<ICacheStore>();
+            var cacheRegistry = Substitute.For<ICacheRegistry>();
+
+            var cache = new Cache(cacheStore, cacheRegistry);
+
+            var key = "key";
+            cache.FetchAsync<TestItem>();
+
+            cacheRegistry.Received(1).TryGetTask(typeof(TestItem).FullName, out ICacheTask _);
+        }
+    }
+}
